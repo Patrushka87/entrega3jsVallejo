4c85688e2bdea8d862dee762a39edc7aab0f9fe4
Daniel Vallejo <daniel.cero.nueve@gmail.com>
2023-03-04 18:36:10 -0300

    entrega

---
+++ data/stock.js
@@ -0,0 +1,101 @@
+const productos =
+[
+       {
+        id: 1,
+        nombre: "Sticker",
+        tipo: "autoadhesivo",
+        desc: "Stickers redondo personalizado",
+        precio: 100,
+        tamaño: "chico",
+        imagen: '../public/images/calco.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 2,
+        nombre: "Poster",
+        tipo: "carton",
+        desc: "Poster grande personalizado",
+        precio: 500,
+        tamaño: "grande",
+        imagen: '../public/images/poster.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 3,
+        nombre: "Cuadro",
+        tipo: "lona",
+        desc:"Cuadro de lona decorativo",
+        precio: 1500,
+        tamaño: "grande",
+        imagen: '../public/images/cuadro.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 4,
+        nombre: "Sticker Pro",
+        tipo: "autoadhesivo",
+        desc: "Sticker resistente para celular",
+        precio: 200,
+        tamaño: "chico",
+        imagen: '../public/images/stickerRes.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 5,
+        nombre: "Tarjetas",
+        tipo: "carton",
+        desc: "tarjetas personales",
+        precio: 2000,
+        tamaño: "chico",
+        imagen: '../public/images/tarjeta.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 6,
+        nombre: "Folletos",
+        tipo: "papel",
+        desc: "Folletos personalizados",
+        precio: 5000,
+        tamaño: "mediano",
+        imagen: '../public/images/folleto.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 7,
+        nombre: "Corporeas",
+        tipo: "polifan",
+        desc: "Tu logo corporeo ",
+        precio: 10000,
+        tamaño: "grande",
+        imagen: '../public/images/corp.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 8,
+        nombre: "Llaveros 3D",
+        tipo: "plastico",
+        desc: "Llaveros impresos en 3D con tu logo",
+        precio: 650,
+        tamaño: "chico",
+        imagen: '../public/images/3d.jpg',
+        cantidad: 1,
+    },
+
+    {
+        id: 9,
+        nombre: "Banner de pie",
+        tipo: "lona",
+        desc: "Banner personalizado para tu empresa",
+        precio: 8500,
+        tamaño: "grande",
+        imagen: '../public/images/banner.jpg',
+        cantidad: 1,
+    },
+];
